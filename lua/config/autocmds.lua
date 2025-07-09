2661d62aa55a4643715e17dad385a46acfb76dc1
Christian Ramet <git@christianramet.com>
2025-07-09 09:51:39 +0200

    feat(autocmds): visual and hard wrap

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,13 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text", "gitcommit", "org" },
+  callback = function()
+    vim.opt_local.wrap = true -- visual wrap
+    vim.opt_local.linebreak = true --  do not cut words
+    -- vim.opt_local.spell = true -- spell checking
+    vim.opt_local.textwidth = 80 -- hard wrap
+  end,
+})
