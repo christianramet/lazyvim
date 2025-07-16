aaa6e389e508f56d1d8fd9e019cbe0eaa2cb8098
Christian Ramet <git@christianramet.com>
2025-07-16 16:24:52 +0200

    feat(keybinds): zet

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Zettelkasten keybinds
+local zet_path = "~/Nextcloud/zet"
+
+vim.keymap.set("n", "<leader>zf", LazyVim.pick("files", { cwd = zet_path }), { desc = "Find Zettel" })
+vim.keymap.set("n", "<leader>zg", LazyVim.pick("live_grep", { cwd = zet_path }), { desc = "Grep Zettel" })
+vim.keymap.set("n", "<leader>zn", function()
+  vim.ui.input({ prompt = "Zettel title: " }, function(title)
+    if not title or title == "" then
+      return
+    end
+    local slug = title:gsub(" ", "-")
+    local filename = string.format("%s/%s-%s.md", zet_path, os.date("%Y%m%d%H%M"), slug)
+    vim.cmd("e " .. filename)
+  end)
+end, { desc = "New Zettel" })
